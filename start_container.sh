60d165219f2ebb959c63670f77c68c9dbd084d38
stgriesb <58586674+stgriesb@users.noreply.github.com>
2021-03-23 16:32:52 +0100

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo docker run -d --network host --name cups --restart always \
+sudo docker run -d --network host --name cups-avahi --restart always \
 -p 631:631/tcp \
--v /volume1/docker/cups/cfg/spool:/var/spool:rw \
--v /volume1/docker/cups/cfg:/etc/cups:rw \
+-v /volume1/docker/cups-avahi/cfg/spool:/var/spool \
+-v /volume1/docker/cups-avahi/cfg:/etc/cups \
 -v /etc/localtime:/etc/localtime:ro \
 -e "CUPS_ADMIN_USERNAME=ADMIN" \
 -e "CUPS_ADMIN_PASSWORD=PASSWORD" \
-cups:latest 
+cups:latest

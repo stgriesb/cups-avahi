fbb9d9b55a9b20cb97a929eb1282001236ba7c66
stgriesb <58586674+stgriesb@users.noreply.github.com>
2021-03-23 16:10:23 +0100

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,8 +2,8 @@
 
 sudo docker run -d --network host --name cups --restart always \
 -p 631:631/tcp \
--v /volume1/docker/cups/cfg/spool:/var/spool:rw \
--v /volume1/docker/cups/cfg:/etc/cups:rw \
+-v /docker/cups/cfg/spool:/var/spool:rw \
+-v /docker/cups/cfg:/etc/cups:rw \
 -v /etc/localtime:/etc/localtime:ro \
 -e "CUPS_ADMIN_USERNAME=ADMIN" \
 -e "CUPS_ADMIN_PASSWORD=PASSWORD" \
